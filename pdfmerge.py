d20fb3126991f8dab20fee983e3b08cfbe5311c8
WagnerWolf <48105783+WagnerWolf@users.noreply.github.com>
2024-02-20 09:17:56 -0300

    Update pdfmerge.py

--- pdfmerge.py
+++ pdfmerge.py
@@ -7,12 +7,9 @@ merger = PyPDF2.PdfMerger()
 output_file = "./output/"+str(datetime.now().timestamp())+".pdf"
 files = [f for f in pathlib.Path('input').iterdir() if f.is_file()]
 files = Tcl().call('lsort', '-dict', files)
+
 for i in files:
     print(i)
     input_file = i
     merger.append(input_file)
-
 merger.write(output_file)
-
-for i in files:
-    rm(i)
