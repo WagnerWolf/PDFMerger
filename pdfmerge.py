d2f5a57e3395d23904f20fb5c5e4063a6d212f90
WagnerWolf <48105783+WagnerWolf@users.noreply.github.com>
2024-02-20 00:08:04 -0300

    Add files via upload

---
+++ pdfmerge.py
@@ -0,0 +1,18 @@
+import PyPDF2
+import pathlib
+from os import remove as rm
+from datetime import datetime
+from tkinter import Tcl
+merger = PyPDF2.PdfMerger()
+output_file = "./output/merged "+str(datetime.now())+".pdf"
+files = [f for f in pathlib.Path('input').iterdir() if f.is_file()]
+files = Tcl().call('lsort', '-dict', files)
+for i in files:
+    print(i)
+    input_file = i
+    merger.append(input_file)
+
+merger.write(output_file)
+
+#for i in files:
+#    rm(i)
